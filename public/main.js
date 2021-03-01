e70039486019198912f2213ff8ff5b6a31044821
ika-si <ikachan.0630@gmail.com>
2021-03-02 00:39:09 +0900

    firebaseの値を表示

--- public/main.js
+++ public/main.js
@@ -1,34 +1,17 @@
 var db = firebase.firestore();
 
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
-
-// Add a second document with a generated ID.
-db.collection("users").add({
-    first: "Alan",
-    middle: "Mathison",
-    last: "Turing",
-    born: 1912
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
-
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
+function getAll() {
+    let collection = db.collection("users").orderBy('createdAt');
+    collection.get().then((querySnapshot) => {
+        $('#list').text('');
+        querySnapshot.forEach((doc) => {
+            if(doc.data()['name'] == "yurika") {
+                $('#list').append('<li class="my">' + doc.data()['createdAt'].toDate() + '<br>' + doc.data()['name'] + '</li>');
+            } else {
+                $('#list').append('<li> class="your">' + doc.data()['createdAt'].toDate() + '<br>' + doc.data()['name'] + '</li>');    
+            }
+        })
+    })
+}
+getAll();
 
